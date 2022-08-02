59f5185c770cff699d617c97e669a8e86dbd2742
guusfelix2015 <gusfelix2015@gmail.com>
2022-08-02 19:58:12 -0300

    Comunicação entre components

--- aulas/01-fundamentos-reactjs/src/components/Comment.jsx
+++ aulas/01-fundamentos-reactjs/src/components/Comment.jsx
@@ -2,7 +2,11 @@ import styles from "./Comment.module.css";
 import { ThumbsUp, Trash } from "phosphor-react";
 import { Avatar } from "./Avatar";
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/LipzDev.png" />
@@ -15,7 +19,7 @@ export function Comment({ content }) {
                 Cerca de uma hora atrás
               </time>
             </div>
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
