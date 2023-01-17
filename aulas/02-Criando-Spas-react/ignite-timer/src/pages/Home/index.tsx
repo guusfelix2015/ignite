ff74eadfbd41e52e2a73f404ce6cb2e216058fee
guusfelix2015 <gusfelix2015@gmail.com>
2023-01-17 18:27:17 -0300

    Iniciando um novo ciclo

--- aulas/02-Criando-Spas-react/ignite-timer/src/pages/Home/index.tsx
+++ aulas/02-Criando-Spas-react/ignite-timer/src/pages/Home/index.tsx
@@ -12,6 +12,7 @@ import {
   StartCountDonwButton,
   TaskInput,
 } from './styles'
+import { useState } from 'react'
 
 const newCicleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
@@ -23,7 +24,16 @@ const newCicleFormValidationSchema = zod.object({
 
 type NewCicleFormData = zod.infer<typeof newCicleFormValidationSchema>
 
+interface Cycle {
+  id: string
+  task: string
+  minutesAmount: number
+}
+
 export function Home() {
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+
   const { register, handleSubmit, watch, reset } = useForm<NewCicleFormData>({
     resolver: zodResolver(newCicleFormValidationSchema),
     defaultValues: {
@@ -33,9 +43,22 @@ export function Home() {
   })
 
   function handleCreateNewCicle(data: NewCicleFormData) {
-    console.log(data)
+    const id = String(new Date().getTime())
+
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+    }
+
+    setCycles((state) => [...state, newCycle])
+    setActiveCycleId(id)
     reset()
   }
+
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  console.log(activeCycle)
 
   const task = watch('task')
   const isSubmitDisabled = !task
