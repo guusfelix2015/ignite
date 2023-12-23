930955694f31c9fdf4c57f79d25d6062d196af53
guusfelix2015 <gusfelix2015@gmail.com>
2023-12-22 22:49:19 -0300

    imagens no next

--- aulas/04-ignite-shop/src/pages/_app.tsx
+++ aulas/04-ignite-shop/src/pages/_app.tsx
@@ -1,9 +1,20 @@
 import { AppProps } from "next/app"
 import { globalStyles } from "../styles/global"
+import { Container, Header } from "../styles/pages/app"
+import Image from "next/image"
+import logoImg from "../assets/logo.svg"
+
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
 
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} width={130} height={52} alt="logo"/>
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
+}
---
+++ aulas/04-ignite-shop/src/styles/pages/app.ts
@@ -0,0 +1,16 @@
+import { styled } from "..";
+
+export const Container = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "flex-start",
+  minHeight: "100vh",
+  justifyContent: "center",
+})
+
+export const Header = styled("header", {
+  width: "100%",
+  padding: "2rem 0",
+  maxWidth: 1110,
+  margin: "0 auto",
+})
