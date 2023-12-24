c261a066fe934ed0ea4555fe71e2d255315da02e
guusfelix2015 <gusfelix2015@gmail.com>
2023-12-23 22:13:54 -0300

    Estrutura da Home

--- aulas/04-ignite-shop/src/pages/index.tsx
+++ aulas/04-ignite-shop/src/pages/index.tsx
@@ -1,9 +1,31 @@
 import { styled } from "../styles"
+import { HomeContainer, Product } from "../styles/pages/home"
+import Image from "next/image"
 
-const Button = styled("button", {
-  backgroundColor: "$green300"
-})
+import camiseta1 from "../assets/camisetas/1.png"
+import camiseta2 from "../assets/camisetas/2.png"
+import camiseta3 from "../assets/camisetas/3.png"
+
+
 
 export default function Home() {
-  return <Button>Enviar</Button>
+  return (
+    <HomeContainer>
+      <Product>
+      <Image src={camiseta2} width={520} height={480} alt="caliseta 2" />
+        <footer>
+          <strong>Camiseta 1</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+      <Image src={camiseta2} width={520} height={480} alt="caliseta 2" />
+        <footer>
+          <strong>Camiseta 1</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      
+    </HomeContainer>
+  )
 }
