56d3efa72b249ded04a072e7b12100f530d2fdad
guusfelix2015 <gusfelix2015@gmail.com>
2023-12-23 22:31:09 -0300

    criando carrossel

--- aulas/04-ignite-shop/src/pages/index.tsx
+++ aulas/04-ignite-shop/src/pages/index.tsx
@@ -1,31 +1,47 @@
-import { styled } from "../styles"
 import { HomeContainer, Product } from "../styles/pages/home"
 import Image from "next/image"
-
-import camiseta1 from "../assets/camisetas/1.png"
+import { useKeenSlider } from "keen-slider/react"
 import camiseta2 from "../assets/camisetas/2.png"
-import camiseta3 from "../assets/camisetas/3.png"
-
-
+import "keen-slider/keen-slider.min.css"
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    }
+  })
   return (
-    <HomeContainer>
-      <Product>
-      <Image src={camiseta2} width={520} height={480} alt="caliseta 2" />
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={camiseta2} width={520} height={480} alt="caliseta 2" />
         <footer>
           <strong>Camiseta 1</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
-      <Product>
-      <Image src={camiseta2} width={520} height={480} alt="caliseta 2" />
+      <Product className="keen-slider__slide">
+        <Image src={camiseta2} width={520} height={480} alt="caliseta 2" />
         <footer>
           <strong>Camiseta 1</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
-      
+      <Product className="keen-slider__slide">
+        <Image src={camiseta2} width={520} height={480} alt="caliseta 2" />
+        <footer>
+          <strong>Camiseta 1</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta2} width={520} height={480} alt="caliseta 2" />
+        <footer>
+          <strong>Camiseta 1</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
     </HomeContainer>
   )
 }
